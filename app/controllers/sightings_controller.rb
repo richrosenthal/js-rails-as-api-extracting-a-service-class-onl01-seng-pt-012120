a33d8888a0505a9a52eebe8260acac19d1a69996
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-11-01 15:41:12 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -2,7 +2,7 @@ class SightingsController < ApplicationController
   def index
    sightings = Sighting.all
    render json: SightingSerializer.new(sightings).to_serialized_json
- end
+ end 
 
  def show
    sighting = Sighting.find_by(id: params[:id])
